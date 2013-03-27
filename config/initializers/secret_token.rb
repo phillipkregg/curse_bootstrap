73a9cb0b93619d2d064cbd3d7079e5b471fa36c2
phillipkregg <phillipkregg@gmail.com>
2013-03-27 00:07:44 -0500

    Initial commit with bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CurseBootstrap::Application.config.secret_token = '5b8eef6970164c9420f0007f0e2deab1d7df91369e0df1945e2f9f31de918baa5066bd5c5dbe19ec4764f6b0f32323eb3d886c7780a5916044ea1b19e304401b'
